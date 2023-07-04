f5c47c42d46e370f7c635ce1a9628126ea4076c0
pretzelogical <yingyangyo266@gmail.com>
2023-07-04 12:06:41 -0700

    fix test & dev sql files

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,4 +2,4 @@
 CREATE DATABASE hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
