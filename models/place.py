a474a68e631919e4d208d57c6ea87964ddc3cfc2
pretzelogical <yingyangyo266@gmail.com>
2023-07-19 14:27:55 -0500

    collate user_id in place

--- models/place.py
+++ models/place.py
@@ -22,7 +22,8 @@ class Place(BaseModel, Base):
     __tablename__ = 'places'
     city_id = Column(String(60, collation="latin1_swedish_ci"),
                      ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    user_id = Column(String(60, collation="latin1_swedish_ci"),
+                     ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
