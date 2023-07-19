9aebc54e91ff69bd099672dc38ceb1269a585328
pretzelogical <yingyangyo266@gmail.com>
2023-07-19 14:37:09 -0500

    replace amenity file

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,28 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-import models
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False)
+                      )
 
 
 class Amenity(BaseModel, Base):
-    """ Class representation of Amenity """
-    __tablename__ = 'amenities'
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary='place_amenity')
+    """ Class attribute place_amenities must represent a relationship
+    Many-To-Many between the class Place and Amenity. Please see below more
+    detail: place_amenity in the Place update """
+    place_amenities = relationship("Place", secondary='place_amenity',
+                                   back_populates="_amenities")
