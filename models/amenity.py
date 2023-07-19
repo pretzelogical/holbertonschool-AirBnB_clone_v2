689922f0b2f5e31f7a6a5e5a4253c641f1ce4a52
pretzelogical <yingyangyo266@gmail.com>
2023-07-19 14:46:33 -0500

    add viewonly=False to place_amenities

--- models/amenity.py
+++ models/amenity.py
@@ -1,28 +1,15 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" Amenity Module for HBNB project """
+import os
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             primary_key=True, nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             primary_key=True, nullable=False)
-                      )
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
+    """ Class representation of Amenity """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    """ Class attribute place_amenities must represent a relationship
-    Many-To-Many between the class Place and Amenity. Please see below more
-    detail: place_amenity in the Place update """
-    place_amenities = relationship("Place", secondary='place_amenity',
-                                   back_populates="_amenities")
+    place_amenities = relationship(
+        "Place", secondary='place_amenity', viewonly=False)
